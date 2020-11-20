1d8ef6933c499a418c8e29617a2b033e82dc8e56
tjmoon0104 <tjmoon0104@outlook.com>
2020-11-20 14:37:04 +0100

    Update

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -47,6 +47,21 @@ class EditContact extends Component {
       return
     }
 
+    const updContact = {
+      name,
+      email,
+      phone,
+    }
+
+    const { id } = this.props.match.params
+
+    const res = await axios.put(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+      updContact
+    )
+
+    dispatch({ type: 'UPDATE_CONTACT', payload: res.data })
+
     this.setState({ name: '', email: '', phone: '', errors: {} })
     this.props.history.push('/')
   }
